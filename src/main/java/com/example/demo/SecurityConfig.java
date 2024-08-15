c46b51af323a5c7fe5bf32c179ad8f73d1cf1ec7
Tanmay Tiwari <tanmaytiwari0508@gmail.com>
2024-08-16 00:03:32 +0530

    basic spring_boot security

---
+++ src/main/java/com/example/demo/SecurityConfig.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((requests) -> requests
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults());  // Enables basic authentication
+        return http.build();
+    }
+}
+
