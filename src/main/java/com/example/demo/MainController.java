cfd1525f181f8112131fd28711629a23e78cfbb2
Tanmay Tiwari <tanmaytiwari0508@gmail.com>
2024-09-22 22:10:16 +0530

    basic JSON controller

--- src/main/java/com/example/demo/MainController.java
+++ src/main/java/com/example/demo/MainController.java
@@ -1,13 +1,14 @@
 package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainController {
     @GetMapping("/main")
-    public String getMe(){
-        return "hello from main class";
+    public String getMein(@RequestBody Student student){
+        return "hello from main class " + student.getName() + " " + student.getAge();
     }
 }
