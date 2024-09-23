282c366d26fc4e659a3a2a6991165b046ede92fc
Tanmay Tiwari <tanmaytiwari0508@gmail.com>
2024-09-23 10:51:10 +0530

    request body in spring boot

--- src/main/java/com/example/demo/MainController.java
+++ src/main/java/com/example/demo/MainController.java
@@ -2,13 +2,14 @@ package com.example.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainController {
     @GetMapping("/main")
-    public String getMein(@RequestBody Student student){
-        return "hello from main class " + student.getName() + " " + student.getAge();
+    public Student getMein(@RequestBody Student student){
+        Student obj = new Student();
+        obj.setName(student.getName()+" is verified");
+        return obj;
     }
 }
