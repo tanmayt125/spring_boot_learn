7472874536453dfbf9afdae02ec03abb6599dd03
Tanmay Tiwari <tanmaytiwari0508@gmail.com>
2024-10-21 19:44:51 +0530

    jsonf parser

---
+++ src/main/java/com/example/demo/Driver.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class Driver {
+    public static void main(String[] args) throws Exception {
+        String jsonResponse = "{\"Name\": \"John is verified\", \"age\": 20}";
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        try {
+            Student student = objectMapper.readValue(jsonResponse, Student.class);
+            System.out.println("Name: " + student.getName());
+            System.out.println("Age: " + student.getAge());
+        } catch (Exception e) {
+            throw new Exception("Issue in Json Parsing");
+        }
+    }
+}
--- src/main/java/com/example/demo/MainController.java
+++ src/main/java/com/example/demo/MainController.java
@@ -13,4 +13,5 @@ public class MainController {
         obj.setName(student.getName()+" is verified");
         return obj;
     }
+
 }
